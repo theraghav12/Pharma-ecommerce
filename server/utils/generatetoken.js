48aad6e99f3c4643a1930e3074a13713e09ec844
theraghav12 <khuranaraghav388@gmail.com>
2024-10-23 00:13:28 +0530

    done some changes

--- server/utils/generatetoken.js
+++ server/utils/generatetoken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d', // Token expires in 30 days
+    expiresIn: '30d', 
   });
 };
 
