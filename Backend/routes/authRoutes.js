27cc758ec36974cccf08a7f16ea96347ca46260a
theraghav12 <khuranaraghav388@gmail.com>
2025-04-08 21:28:02 +0530

    updated eslintrc

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, getDoctors, getUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.get("/doctors", getDoctors);     // GET all doctors
+router.get("/:id", getUser);            
 export default router;
